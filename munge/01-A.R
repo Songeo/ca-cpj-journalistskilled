ee074e00c38d4df4383e68efb8c43828f0b9f65d
Songeo <sonia.gmcl@gmail.com>
2018-02-08 20:02:13 -0600

    actualizacion de datos

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,5 +1,7 @@
 # Example preprocessing script.
 
+
+# Datos hasta 2017 ----
 tt <- read_csv("data/cpj_mods.csv")
 names(tt) <- tolower(names(tt))
 
@@ -21,13 +23,45 @@ df_cpj %>%
             max(date_registered, na.rm = T)) %>% 
   data.frame()
 
+
+
+# df_cpj$coverage %>% table(useNA = "always")
+# df_cpj$`source of fire` %>% table(useNA = "always")
+# df_cpj$`type of death` %>% table(useNA = "always")
+# df_cpj$`impunity (for murder)` %>% table(useNA = "always")
+# df_cpj$`taken captive` %>% table(useNA = "always")
+# df_cpj$tortured %>% table(useNA = "always")
+# df_cpj$threatened %>% table(useNA = "always")
+
 rm("tt")
 
-df_cpj$coverage %>% table(useNA = "always")
-df_cpj$`source of fire` %>% table(useNA = "always")
-df_cpj$`type of death` %>% table(useNA = "always")
-df_cpj$`impunity (for murder)` %>% table(useNA = "always")
-df_cpj$`taken captive` %>% table(useNA = "always")
-df_cpj$tortured %>% table(useNA = "always")
-df_cpj$threatened %>% table(useNA = "always")
+
+
+# Datos nuevos commited ----
+
+# tt <- read_csv("data/Killed_Confirmed_92_18.csv")
+# dim(tt)
+# tt %>% data.frame() %>% head
+# 
+# apply(is.na(tt), 2, sum)
+
+df_cpj_confirmed <- read_csv("data/Killed_Confirmed_92_18.csv") %>% 
+  dplyr::select(id, href, name = fullName, personId,
+                motive = motiveConfirmed,
+                primaryNationality, 
+                year, startDate, startDisplay,
+                impunity, 
+                gender, type,
+                localOrForeign,
+                sourcesOfFire, typeOfDeath,
+                tortured, captive, threatened,
+                jobs, employedAs, 
+                mediums, coverages,
+                country, country_name, countryShort, region_short) %>% 
+  mutate(date_registered = mdy(startDate),
+         month_year = dmy(paste0("01-", format(date_registered, "%m-%Y")))) %>% 
+  filter(!is.na(name)) %>% 
+  arrange(desc(date_registered))
+df_cpj_confirmed
+
 
